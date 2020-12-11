694b7d85cd452aec62780dd7a671ff7a8a9f46fe
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2020-12-11 18:27:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import sys
 
+
 def main():
     name = input("Enter Name : ")
     height_m = input("Height (m) : ")
@@ -11,14 +12,18 @@ def main():
     def bmi_calculator(name, height_m, weight_kg):
         bmi = weight_kg / (height_m ** 2)
         print()
-        print("BMI :",bmi)
-        if bmi < 25:
-            return name + " is not overweight"
+        print("BMI :", bmi)
+        if bmi < 18.5:
+            return name + " is Underweight"
+        elif bmi >= 18.5 and bmi <= 24.9:
+            return name + " is Normal weight"
+        elif bmi >= 25 and bmi <= 29.9:
+            return name + " is Overweight"
         else:
-            return name + " is OVERWEIGHT"
+            return name + " is Obesity"
 
     result = bmi_calculator(name, height_m, weight_kg)
-    print (result)
+    print(result)
     print()
     print('''Press ENTER to check again
     Press "q" to quit :''')
@@ -28,5 +33,6 @@ def main():
         sys.exit(0)
     else:
         main()
+
 
 main()
